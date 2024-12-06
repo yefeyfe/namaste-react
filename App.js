9de1479452caac33f093f3c09d8a905fb40d982f
yefeyfe <pintu.se.ju@gmail.com>
2024-12-06 10:32:35 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * <div id="parent">
+ *  <div id="child1">
+ *      <h1>I'm an h1 tag</h1>
+ *      <h2>I'm an h2 tag</h2>
+ *  </div>
+ *  <div id="child2">
+ *      <h1>I'm an h1 tag</h1>
+ *      <h2>I'm an h2 tag</h2>
+ *  </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement(
+      "div",
+      {
+        id: "child1",
+        key: "child1",
+      },
+      [
+        React.createElement("h1", { key: "h1" }, "I'm an h1 tag"),
+        React.createElement("h2", { key: "h2" }, "I'm an h2 tag"),
+      ]
+    ),
+    React.createElement(
+      "div",
+      {
+        id: "child2",
+        key: "child2",
+      },
+      [
+        React.createElement("h1", { key: "h1" }, "I'm an h1 tag"),
+        React.createElement("h2", { key: "h2" }, "I'm an h2 tag"),
+      ]
+    ),
+  ]
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
